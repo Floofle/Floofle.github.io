20ee4467892c899a4453d1f447814629ff93395e
Jason Woo <51433091+Floofle@users.noreply.github.com>
2023-04-08 13:12:23 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -109,12 +109,12 @@ export default {
           "mech": "This Set Of Lies"
         },
         {
-          "callsign": "Kal",
-          "alias": "Vier Flau",
+          "callsign": "Sirin",
+          "alias": "Yevdokiya Liliana Kimura",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Necrophile"
+          "mech": "Give Me A Reason"
         },
         {
           "callsign": "Starboard",
